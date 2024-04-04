78a8c2f530aa487e3602e6ac8e51c52e3688408f
Ski <skicodeoo7@gmail.com>
2024-04-05 05:06:22 +0530

    eweqw

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,5 +1,6 @@
 import {BrowserRouter ,Routes,Route} from 'react-router-dom';
 import Home from './Pages/Home'
+import Profile from './Pages/Profile';
 import React from 'react'
 
 const App = () => {
@@ -9,6 +10,8 @@ const App = () => {
       <Routes>
         <Route>
         <Route path="/" element={<Home />} />
+        <Route path="/Profile" element={<Profile />} />
+        
         </Route>
       </Routes>
       </BrowserRouter>
